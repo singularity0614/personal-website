e022c03f4ab296c463512b8bcb625de75e13c849
singularity0614 <bingbongrice@gmail.com>
2022-07-27 22:07:12 +1000

    added wip page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/views/index.html'));
+    res.sendFile(path.join(__dirname, '/src/views/workingon.html'));
 });
 
 app.get('/about', (req, res) => {
