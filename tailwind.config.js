dcf7403c305c81034a35f22e0c5bee9255a4f9be
singularity0614 <bingbongrice@gmail.com>
2022-07-28 23:34:16 +1000

    added fade in effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     extend: {
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
+      },
+      keyframes: {
+        'fade-in-from-bottom': {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(0, 10px) scale(1)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translate(0, 0) scale(1)',
+          }
+        }
+      },
+      animation: {
+        'fade-in-from-bottom': 'fade-in-from-bottom 800ms cubic-bezier(0.4, 0, 0.25, 1)',
       }
     },
   },
